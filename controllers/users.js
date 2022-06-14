d2836ff015be6222bed48d93a2b54e272bfeda33
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-06-14 15:15:45 -0400

    User authentication and creation is working

--- controllers/users.js
+++ controllers/users.js
@@ -1,92 +1,89 @@
 ////////////////////////////////////////
 // Import Dependencies
 ////////////////////////////////////////
-const express = require("express");
-const User = require("../models/user");
-// const bcrypt = require("bcryptjs");
+const express = require("express")
+const User = require("../models/user")
+const bcrypt = require("bcryptjs")
 
 /////////////////////////////////////////
 // Create Route
 /////////////////////////////////////////
-const router = express.Router();
+const router = express.Router()
 
 /////////////////////////////////////////
 // Routes
 /////////////////////////////////////////
+// The login Routes (Get => form, post => submit form)
+router.get("/login", (req, res) => {
+    res.render("users/login")
+})
+
+router.post("/login", async (req, res) => {
+    // get the data from the request body
+    const { username, password } = req.body
+    // search for the user
+    User.findOne({ username })
+        .then(async (user) => {
+            // check if user exists
+            if (user) {
+                // compare password
+                const result = await bcrypt.compare(password, user.password)
+                if (result) {
+                    // store some properties in the session object
+                    req.session.username = username
+                    req.session.loggedIn = true
+                    // redirect to fruits page if successful
+                    res.redirect("/albums", {
+                        username
+                    })
+                } else {
+                    // error if password doesn't match
+                    res.json({ error: "password doesn't match" })
+                }
+            } else {
+                // send error if user doesn't exist
+                res.json({ error: "user doesn't exist" })
+            }
+        })
+        .catch((error) => {
+            // send error as json
+            console.log(error)
+            res.json({ error })
+        })
+})
 
 // The Signup Routes (Get => form, post => submit form)
 router.get("/signup", (req, res) => {
-  res.render("users/signup");
-});
-
-router.post("/signup", (req, res) => {
-  res.send("signup");
-});
-
-// The login Routes (Get => form, post => submit form)
-router.get("/login", (req, res) => {
-  res.render("users/login");
-});
+    res.render("users/signup")
+})
 
 router.post("/signup", async (req, res) => {
-  // encrypt password
-  req.body.password = await bcrypt.hash(
-    req.body.password,
-    await bcrypt.genSalt(10)
-  );
-  // create the new user
-  User.create(req.body)
-    .then((user) => {
-      // redirect to login page
-      res.redirect("/users/login");
-    })
-    .catch((error) => {
-      // send error as json
-      console.log(error);
-      res.json({ error });
-    });
-});
-
-router.post("/login", async (req, res) => {
-  // get the data from the request body
-  const { username, password } = req.body;
-  // search for the user
-  User.findOne({ username })
-    .then(async (user) => {
-      // check if user exists
-      if (user) {
-        // compare password
-        const result = await bcrypt.compare(password, user.password);
-        if (result) {
-          // store some properties in the session object
-          req.session.username = username;
-          req.session.loggedIn = true;
-          // redirect to fruits page if successful
-          res.redirect("/fruits");
-        } else {
-          // error if password doesn't match
-          res.json({ error: "password doesn't match" });
-        }
-      } else {
-        // send error if user doesn't exist
-        res.json({ error: "user doesn't exist" });
-      }
-    })
-    .catch((error) => {
-      // send error as json
-      console.log(error);
-      res.json({ error });
-    });
-});
+    // encrypt password
+    req.body.password = await bcrypt.hash(
+        req.body.password,
+        await bcrypt.genSalt(10)
+    )
+    // create the new user
+    User.create(req.body)
+        .then((user) => {
+            // redirect to login page
+            res.redirect("/users/login")
+        })
+        .catch((error) => {
+            // send error as json
+            console.log(error)
+            res.json({ error })
+        })
+})
 
 router.get("/logout", (req, res) => {
-  // destroy session and redirect to main page
-  req.session.destroy((err) => {
-    res.redirect("/");
-  });
-});
+    // destroy session and redirect to main page
+    req.session.destroy((err) => {
+        res.redirect("/")
+    })
+})
 
 //////////////////////////////////////////
 // Export the Router
 //////////////////////////////////////////
-module.exports = router;
+module.exports = router
