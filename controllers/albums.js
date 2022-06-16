a5ad1122c1dd4c16cf2250adbc240ec43e8a8dc8
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-06-16 11:59:31 -0400

    Index page now chooses from 1 of 500 artists to display up to 3 of their albums

--- controllers/albums.js
+++ controllers/albums.js
@@ -28,104 +28,63 @@ router.use((req, res, next) => {
 /////////////////////////////////////////
 // index route
 router.get("/", (req, res) => {
-    // Determine genre
-    const artistURL = `https://api.deezer.com/genre/152/artists`
+    // Determine artist
+    const artistId = Math.floor(Math.random() * 500) + 1
+    const artistURL = `https://api.deezer.com/artist/${artistId}`
 
     // Fetch data on all artists from selected genre
     fetch(artistURL)
         .then((apiResponse) => {
             return apiResponse.json()
         })
-        .then((jsd) => {
-            const artistsData = jsd.data
-            // Determine artist
-            const artistId = 9
-            const artistName = 'Coldplay'
-            const requestURL = `https://api.deezer.com/artist/${artistId}/albums`
-
-            // setTimeout(() => {
-
-            // }, 1000);
+        .then((artistData) => {
+            const albumsURL = `https://api.deezer.com/artist/${artistId}/albums`
 
             // Fetch data on all albums from selected artist
-            fetch(requestURL)
+            fetch(albumsURL)
                 .then((apiResponse) => {
                     return apiResponse.json()
                 })
                 .then((jsonData) => {
                     const albumsData = jsonData.data
-                    console.log("here is the albums data: ", albumsData)
+                    let currentAlbumIndex
+                    let selectedAlbums = []
+                    // console.log('current index/artist being tried is: ' + artistId + ', ' + artistData.name)
 
-
-                    let currentArtistIndex = -1
-                    let temp2
-                    let numberOfAlbums
-                    let temp = []
-
-                    // While an artist hasn't yet been selected
-                    // while (currentArtistIndex < 0) {
-                    numberOfAlbums = 0
-                    temp2 = Math.floor(Math.random() * albumsData.length)
-
-                    temp2 = artistId
-
-                    console.log('current index being tried is: ' + temp2 + ', ' + artistsData[temp2].name)
+                    // For up to 50 times,
                     for (let i = 0; i < 50; i++) {
-                        console.log('current number of albums: ' + numberOfAlbums)
-                        console.log('current release being tried: ' + albumsData[i].record_type + ', ' + albumsData[i].title)
-                        if (albumsData[i].record_type === 'album') {
-                            numberOfAlbums++
-                            if (!temp.includes(albumsData[i])) {
-                                temp.push(albumsData[i])
+                        // console.log('current number of albums: ' + selectedAlbums.length)
+                        currentAlbumIndex = Math.floor(Math.random() * albumsData.length)
+                        // console.log('current release being tried: ' + albumsData[currentAlbumIndex].record_type + ', ' + albumsData[currentAlbumIndex].title)
+
+                        // Add the current release to selectedAlbums if the release is type 'album'
+                        // and hasn't already been selected
+                        if (albumsData[currentAlbumIndex].record_type === 'album') {
+                            if (!selectedAlbums.includes(albumsData[currentAlbumIndex])) {
+                                selectedAlbums.push(albumsData[currentAlbumIndex])
                             }
-                            if (numberOfAlbums > 2) {
-                                currentArtistIndex = temp2
+                            if (selectedAlbums.length > 2) {
                                 break
                             }
                         }
                     }
 
-
-
-                    // const albumData = albumsData.data
-                    // let temp = []
-                    // let currentAlbumIndex
-                    //until there are 3 values in temp array
-                    // while (temp.length < 3) {
-                    // if the current album is an "album"
-                    // currentAlbumIndex = Math.floor(Math.random() * albumsData.length)
-                    // console.log('current album index is: ' + currentAlbumIndex)
-                    // if (albumsData.data[currentAlbumIndex].record_type) {
-                        // === 'album') {
-                        // add it to the temp array if it's not already in it
-                        // if (!temp.includes(albumsData.data[currentAlbumIndex])) {
-                            // temp.push(albumsData.data[currentAlbumIndex])
-                            // console.log(temp)
-                    //     }
-                    // }
-                    // }
-                    console.log(temp.length)
-                    const albumData = temp
-                    // console.log("here is the data: ", albumData[0].record_type)
-                    // res.render('albums', {albumData})
                     let favoriteAlbums
-                    const currentUser = User.findOne({ name: req.session.username }, (error, user) => {
+                    User.findOne({ name: req.session.username }, (error, user) => {
                         if (error) {
                             console.log(error)
                         }
                         else {
-                            // console.log(user)
                             favoriteAlbums = user.favorites
                             res.render('albums', {
-                                data: albumData,
-                                artistName,
+                                data: selectedAlbums,
+                                // artistName,
                                 username: req.session.username,
                                 favorites: favoriteAlbums
                             })
                         }
                     })
                 })
-
         })
         .catch((error) => {
             console.log(error)
